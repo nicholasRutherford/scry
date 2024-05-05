00e7fc0ef2cf4d41c6e0a2d313f7992f69a22ae4
Niko Rutherford <zgithub@isomorphism.ca>
2024-05-05 07:29:54 -0700

    Remove unused test page

--- app/meow/page.tsx
+++
@@ -1,8 +0,0 @@
-import { auth } from "@/auth";
-import Navigation from "@/components/navigation";
-
-export default async function Meow() {
-  const session = await auth();
-
-  return <main className="">{session?.user?.email}</main>;
-}
